03d87a16d2faef1e009bc5fd05c15e5bb319d4a8
Giorgio Bonardi <49915877+GiorgioBonardi@users.noreply.github.com>
2023-05-10 11:26:25 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(name='up_ibacop',
+      version='0.0.1',
+      description='up_ibacop',
+      long_description=long_description,
+      long_description_content_type ="text/markdown",
+      author='Isabel Cenamor and Tomas de la Rosa and Fernando Fernandez',
+      author_email='icenamorg@gmail.com',
+      packages=["up_ibacop"],
+      install_requires=["platform_system == 'Linux'"],
+      python_requires='>=3.7',
+      license='APACHE')
