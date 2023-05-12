041a72a4becfc3e0b46f2a097f8cc614e29d5fdc
Giorgio Bonardi <49915877+GiorgioBonardi@users.noreply.github.com>
2023-05-12 12:07:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
-setup(name='up_ibacop',
-      version='0.0.1',
+setup(
+      name='up_ibacop',
+      version='0.0.2',
       description='up_ibacop',
       author='Isabel Cenamor and Tomas de la Rosa and Fernando Fernandez',
       author_email='icenamorg@gmail.com',
       packages=["up_ibacop"],
-      install_requires=["platform_system == 'Linux'"],
       python_requires='>=3.7',
-      license='APACHE')
+      license='APACHE',
+      classifiers=[
+        "Operating System :: POSIX :: Linux"
+      ]
+)
