eb67b7bf17d9a68cf4b810e4170ee0538b7e9de4
Giorgio Bonardi <49915877+GiorgioBonardi@users.noreply.github.com>
2023-05-16 00:03:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='up_ibacop',
-      version='0.0.2',
+      version='0.0.3',
       description='up_ibacop',
       author='Isabel Cenamor and Tomas de la Rosa and Fernando Fernandez',
       author_email='icenamorg@gmail.com',
